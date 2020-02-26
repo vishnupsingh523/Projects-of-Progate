d6627b489eba3e60ae0fe0e762303f28d175ea72
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-02-26 20:18:25 +0530

    Dom-manipulation-getElementByTags files are created

---
+++ DOM-Manipulations/DOM-1.js
@@ -0,0 +1,19 @@
+function changethefont()
+{
+    var para=document.getElementsByTagName("p");
+    para[0].style.fontSize='50px'
+    para[1].style.color="blue"
+    para[2].style.fontFamily="sans-serif"
+    para[3].style.fontStyle="italic"
+    para[4].style.fontVariant="small-caps"
+    
+}
+
+function changethename(){
+    var name= document.getElementsByTagName("span");
+    name[0].innerText="VISHNU"
+    name[1].innerText="VISHNU"
+    name[2].innerText="VISHNU"
+    name[3].innerText="VISHNU"
+    name[4].innerText="VISHNU"
+}
